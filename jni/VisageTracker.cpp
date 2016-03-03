a876e527babf1c2c231556449b832d80c9c82944
E-man <ebatis@outlook.com>
2016-03-03 17:36:10 +0100

    * Added config files to the assets folder

--- jni/VisageTracker.cpp
+++ jni/VisageTracker.cpp
@@ -1,13 +1,19 @@
 #include <jni.h>
 
+// VisageSDK Includes
+#include <VisageTracker2.h>
+
 extern "C"
 {
 	JNIEXPORT void JNICALL Java_com_visage_visagetracker_MainActivity_trackerInit(JNIEnv *env, jstring configFilename);
 }
 
+VisageSDK::VisageTracker2 *tracker;
+
 JNIEXPORT void JNICALL Java_com_visage_visagetracker_MainActivity_trackerInit(JNIEnv *env, jstring configFilename)
 {
 	const char *_configFilename = env->GetStringUTFChars(configFilename, 0);
+	tracker = new VisageSDK::VisageTracker2(_configFilename);
 
 
 }
