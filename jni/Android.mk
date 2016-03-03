16a1989352256575247085b89a90304d949f3f3b
E-man <ebatis@outlook.com>
2016-03-03 16:31:01 +0100

    FUCK YOU ECLIPSE!

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,51 +1,8 @@
 LOCAL_PATH := $(call my-dir)
-         
-include $(CLEAR_VARS)
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-VISAGE_LIBS := ../../../../lib/armeabi-v7a
-endif
-ifeq ($(TARGET_ARCH_ABI),x86)
-VISAGE_LIBS := ../../../../lib/x86
-endif
-LOCAL_MODULE := VisageVision
-LOCAL_SRC_FILES := $(VISAGE_LIBS)/libVisageVision.so
-include $(PREBUILT_SHARED_LIBRARY)
-     
-include $(CLEAR_VARS)
- 
-OPENCV_LIB_TYPE := STATIC
-OPENCV_CAMERA_MODULES := on
-                
-include ../includeOpenCV.mk
-ifeq ("$(wildcard $(OPENCV_MK_PATH))","")
-	#try to load OpenCV.mk from default install location
-	include $(TOOLCHAIN_PREBUILT_ROOT)/user/share/OpenCV/OpenCV.mk
-else
-	include $(OPENCV_MK_PATH)
-endif
- 
-VISAGE_HEADERS  := ../../../include
 
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-VISAGE_LIBS := ../../../lib/armeabi-v7a
-endif
-ifeq ($(TARGET_ARCH_ABI),x86)
-VISAGE_LIBS := ../../../lib/x86
-endif
-       
-LOCAL_C_INCLUDES +=  $(VISAGE_HEADERS) $(VISAGE_HEADERS)/libAfm $(VISAGE_HEADERS)/libVRMLParser $(VISAGE_HEADERS)/../source/libVision/swr
+include $(CLEAR_VARS)
+
 LOCAL_MODULE    := VisageTracker
 LOCAL_SRC_FILES := VisageTracker.cpp
-
-LOCAL_SHARED_LIBRARIES := VisageVision	
-LOCAL_LDLIBS +=  -L$(VISAGE_LIBS) -L$(/jni) -lVisageVision -lGLESv1_CM -llog -ldl -Wl,--gc-sections  
-
-LOCAL_CFLAGS := -DANDROID_NDK \
-				-DDISABLE_IMPORTGL \
-				-DANDROID \
-				-DVISAGE_STATIC \
-				-ffast-math -O2 -funroll-loops -Wno-write-strings
-
-LOCAL_ARM_MODE := arm
 
 include $(BUILD_SHARED_LIBRARY)
--- jni/VisageTracker.cpp
+++ jni/VisageTracker.cpp
@@ -1,19 +1,3 @@
 #include <jni.h>
 
-#include "VisageTracker2.h"
-
-#include <android/log.h>
-#define  LOG_TAG    "TrackerWrapper"
-#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-
-extern "C"
-{
-	JNIEXPORT jstring Java_com_visage_visagetracker_MainActivity_getJNIString(JNIEnv *env, jobject obj);
-}
-
-JNIEXPORT jstring Java_com_visage_visagetracker_MainActivity_getJNIString(JNIEnv *env, jobject obj)
-{
-	return env->NewStringUTF("Suck it to me!!");
-}
-
+jstring blah;
