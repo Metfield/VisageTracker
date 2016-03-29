c26c76ac855369d944c9208b351b18ddbaac66a5
Gusten <johan.gusten@gmail.com>
2016-03-29 16:25:33 +0200

    Adds Jones model and adds loading of model bindings

--- jni/Android.mk
+++ jni/Android.mk
@@ -40,7 +40,7 @@ LOCAL_SRC_FILES := VisageTracker.cpp \
 				   NativeTrackerRenderer.cpp
 
 	LOCAL_SHARED_LIBRARIES := VisageVision	
-	LOCAL_LDLIBS +=  -L$(VISAGE_LIBS) -L$(/jni) -lVisageVision -lGLESv1_CM -llog -ldl -Wl,--gc-sections  
+	LOCAL_LDLIBS +=  -landroid -L$(VISAGE_LIBS) -L$(/jni) -lVisageVision -lGLESv1_CM -llog -ldl -Wl,--gc-sections  
 	
 	LOCAL_CFLAGS := -DANDROID_NDK \
 					-DDISABLE_IMPORTGL \
--- jni/VisageTracker.cpp
+++ jni/VisageTracker.cpp
@@ -1,16 +1,23 @@
 #include <_VisageTracker.h>
-
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+#include <string>
 // VisageSDK Includes
 #include <VisageTracker2.h>
 
 using namespace VisageSDK;
 
 VisageTracker2 *tracker;
+AAssetManager *aMgr;
 
-JNIEXPORT void JNICALL Java_com_visage_visagetracker_MainActivity_trackerInit(JNIEnv *env, jobject obj, jstring configFilename)
+JNIEXPORT void JNICALL Java_com_visage_visagetracker_MainActivity_trackerInit(JNIEnv *env, jobject obj, jstring configFilename, jobject assetManager)
 {
+	// Init tracker
 	const char *_configFilename = env->GetStringUTFChars(configFilename, 0);
 	tracker = new VisageTracker2(_configFilename);
+
+	// Init asset mananger
+	aMgr = AAssetManager_fromJava(env, assetManager);
 
 	// Initialize camera and what not
 }
@@ -23,5 +30,29 @@ void Java_com_visage_visagetracker_JavaCamTracker_WriteFrameCamera(JNIEnv *env, 
 
 JNIEXPORT void JNICALL Java_com_visage_visagetracker_MainActivity_setupBinding(JNIEnv *env, jobject obj, jstring bindFilename)
 {
+	// Load asset using android asset manager
+	const char *_bindFilename = env->GetStringUTFChars(bindFilename, 0);
+	AAsset *bindingsAsset = AAssetManager_open(aMgr, _bindFilename, AASSET_MODE_UNKNOWN);
+	if(bindingsAsset == NULL) {
+		LOGE("Could not find model bindings file");
+	}
+	off_t assetLength = AAsset_getLength(bindingsAsset);
 
+	// Allocate buffer for our binding data
+	char *buffer = (char*) malloc(assetLength + 1);
+
+	if(AAsset_read(bindingsAsset, buffer, assetLength) < 0) {
+		LOGE("Error loading bindings data in the asset manager");
+		return;
+	}
+
+	// Zero-terminate
+	buffer[assetLength] = 0;
+
+	// Asset has been loaded into memory, close asset.
+	AAsset_close(bindingsAsset);
+
+	std::string bindingsData(buffer);
+
+	// TODO: parse buffer data into something useful
 }
